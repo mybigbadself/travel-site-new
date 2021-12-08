dd6a9b2e4e9a97e2eb75fca2a5f4f635ca762b3f
Alan McCabe <mccabe.alan@gmail.com>
2021-12-08 14:16:14 +0000

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const { watch } = require('fs')
+const path = require('path')
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app' ) 
+    },
+    mode: 'development',
+    watch: true
+}
