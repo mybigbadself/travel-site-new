8a3f05e88c6a44adeb0693e8909e89f2f61acb89
Alan McCabe <mccabe.alan@gmail.com>
2021-12-14 20:51:38 +0000

    Sticky Header complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import '../styles/styles.css';
-import MobileMenu from './modules/MobileMenu'; 
+import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
 
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 75);
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 60);
+let stickyHeader = new StickyHeader();
 
 if (module.hot) {
     module.hot.accept();
