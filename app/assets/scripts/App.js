91e33cd37c8728c067e84726376d84ab150e5cbe
Alan McCabe <mccabe.alan@gmail.com>
2021-12-15 13:55:24 +0000

    Modal Complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,33 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+// import Modal from './modules/Modal';
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
-
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 75);
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 60);
 let stickyHeader = new StickyHeader();
+//new Modal();
+
+let modal
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault()
+        if (typeof modal == 'undefined') {
+            import(/*webpackChunkName: 'Modal' */ './modules/Modal')
+                .then(x => {
+                    modal = new x.default() // creates new instance of modal class.
+                    
+                    setTimeout(() => modal.openTheModal(), 20)
+                })
+                .catch(() => console.log('there was a problem!'))            
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept();
